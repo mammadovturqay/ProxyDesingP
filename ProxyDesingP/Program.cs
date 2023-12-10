28c6feab2f6e79f711cf8da57d42c81125fd7c68
= <=Turqay@mail.ru>
2023-12-10 13:48:50 +0400

    Add project files.

---
+++ ProxyDesingP/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Proxy
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            Proxy proxy = new Proxy();
+            proxy.Request();
+
+
+            Console.ReadKey(); //Codu saxlama
+        }
+    }
+
+
+
+    public abstract class Subject
+    {
+        public abstract void Request();
+    }
+
+
+
+    public class RealSubject : Subject
+    {
+        public override void Request()
+        {
+            Console.WriteLine(" RealSubjectn Request");
+        }
+    }
+
+
+
+    public class Proxy : Subject
+    {
+        private RealSubject ?realSubject;
+
+        public override void Request()
+        {
+
+            if (realSubject == null)
+            {
+                realSubject = new RealSubject();
+            }
+
+            realSubject.Request();
+        }
+    }
+}
+
